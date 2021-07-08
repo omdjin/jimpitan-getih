5772e97b74d10444c4a065be547c9bd6ec68ef8f
Farizal Tri Anugrah <omdjin@gmail.com>
2021-07-08 23:38:03 +0700

    fix: fix case sensitive file name

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import Layout from "@/components/Layout";
+import Layout from "@/components/MainLayout";
 import { supabase } from "@/lib/suppabase";
 import "@/styles/globals.css";
 
