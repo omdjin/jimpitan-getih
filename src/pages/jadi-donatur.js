a2f83c38d5a059efb3b33cecd31979a51d5c48cf
Farizal Tri Anugrah <omdjin@gmail.com>
2021-07-17 23:38:33 +0700

    improvement(jadi-donatur): simplify formik

--- src/pages/jadi-donatur.js
+++ src/pages/jadi-donatur.js
@@ -1,11 +1,23 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import * as yup from 'yup';
 import { Button, Col, Form, InputGroup } from 'react-bootstrap';
 
 const requiredMsg = 'Harus diisi';
+const INITIAL_VALUES = {
+  name: '',
+  age: '',
+  phone: '',
+  blood: '',
+  min_weight: false,
+  sex: '',
+  positive_covid: false,
+  recovered_covid: '',
+  is_healthy: '',
+  is_ready_donor: ''
+};
 
 const schema = yup.object().shape({
   name: yup.string().required(requiredMsg),
@@ -28,6 +40,20 @@ const schema = yup.object().shape({
 });
 
 const Home = () => {
+  const handleSubmitForm = (values, { resetForm, setSubmitting }) => {
+    setTimeout(() => {
+      console.log('masih on progress');
+      setSubmitting(false);
+      resetForm();
+      scrollTo(0, 0);
+    }, 400);
+  };
+
+  const formik = useFormik({
+    validationSchema: schema,
+    onSubmit: handleSubmitForm,
+    initialValues: INITIAL_VALUES
+  });
   return (
     <>
       <Head>
@@ -38,259 +64,234 @@ const Home = () => {
       <div className="text-center">
         <h1>Jadi Donatur Plasma Konvalesens</h1>
         <p>Jika anda memenuhi persyaratan di bawah, mohon isi formulir di bawah ini</p>
-        <p>
-          <Image
-            src="/assets/IG-PLASMA-31.jpg"
-            alt="syarat"
-            width={590}
-            height={600}
-            layout="intrinsic"
-          />
-        </p>
+        <Image
+          src="/assets/IG-PLASMA-31.jpg"
+          alt="syarat"
+          width={590}
+          height={600}
+          layout="intrinsic"
+        />
       </div>
-      <Formik
-        validationSchema={schema}
-        onSubmit={(values, { resetForm, setSubmitting }) => {
-          setTimeout(() => {
-            console.log(values);
-            setSubmitting(false);
-            resetForm();
-          }, 400);
-        }}
-        initialValues={{
-          name: '',
-          age: '',
-          phone: '',
-          blood: '',
-          min_weight: false,
-          sex: '',
-          positive_covid: false,
-          recovered_covid: '',
-          is_healthy: '',
-          is_ready_donor: ''
-        }}>
-        {({ handleSubmit, handleChange, handleBlur, values, touched, isValid, errors }) => {
-          return (
-            <Form noValidate onSubmit={handleSubmit}>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>Nama Anda</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="name"
-                  value={values.name}
-                  onChange={handleChange}
-                  isValid={touched.name && !errors.name}
-                  isInvalid={!!errors.name}
-                />
-                <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>Umur / Usia</Form.Label>
-                <InputGroup>
-                  <Form.Control
-                    type="number"
-                    name="age"
-                    value={values.age}
-                    onChange={handleChange}
-                    isValid={touched.age && !errors.age}
-                    isInvalid={!!errors.age}
-                  />
-                  <InputGroup.Text>Tahun</InputGroup.Text>
-                  <Form.Control.Feedback type="invalid">{errors.age}</Form.Control.Feedback>
-                </InputGroup>
-              </Form.Group>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>Nomor / Whatsapp</Form.Label>
-                <Form.Control
-                  type="number"
-                  name="phone"
-                  value={values.phone}
-                  onChange={handleChange}
-                  isValid={touched.phone && !errors.phone}
-                  isInvalid={!!errors.phone}
-                />
-                <Form.Control.Feedback type="invalid">{errors.phone}</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>Golongan Darah</Form.Label>
-                <div>
-                  <Form.Check
-                    onChange={handleChange}
-                    id="blood-a"
-                    type="radio"
-                    name="blood"
-                    label="A"
-                    value="A"
-                    isInvalid={!!errors.blood}
-                    checked={values.blood === 'A'}
-                  />
-                  <Form.Check
-                    onChange={handleChange}
-                    id="blood-b"
-                    type="radio"
-                    name="blood"
-                    label="B"
-                    value="B"
-                    isInvalid={!!errors.blood}
-                    checked={values.blood === 'B'}
-                  />
-                  <Form.Check
-                    onChange={handleChange}
-                    id="blood-ab"
-                    type="radio"
-                    name="blood"
-                    label="AB"
-                    value="AB"
-                    isInvalid={!!errors.blood}
-                    checked={values.blood === 'AB'}
-                  />
-                  <Form.Check
-                    onChange={handleChange}
-                    id="blood-o"
-                    type="radio"
-                    name="blood"
-                    label="O"
-                    value="O"
-                    isInvalid={!!errors.blood}
-                    checked={values.blood === 'O'}
-                  />
+      <Form noValidate onSubmit={formik.handleSubmit}>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>Nama Anda</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            isValid={formik.touched.name && !formik.errors.name}
+            isInvalid={!!formik.errors.name}
+          />
+          <Form.Control.Feedback type="invalid">{formik.errors.name}</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>Umur / Usia</Form.Label>
+          <InputGroup>
+            <Form.Control
+              type="number"
+              name="age"
+              value={formik.values.age}
+              onChange={formik.handleChange}
+              isValid={formik.touched.age && !formik.errors.age}
+              isInvalid={!!formik.errors.age}
+            />
+            <InputGroup.Text>Tahun</InputGroup.Text>
+            <Form.Control.Feedback type="invalid">{formik.errors.age}</Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>Nomor / Whatsapp</Form.Label>
+          <Form.Control
+            type="number"
+            name="phone"
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            isValid={formik.touched.phone && !formik.errors.phone}
+            isInvalid={!!formik.errors.phone}
+          />
+          <Form.Control.Feedback type="invalid">{formik.errors.phone}</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>Golongan Darah</Form.Label>
+          <div>
+            <Form.Check
+              onChange={formik.handleChange}
+              id="blood-a"
+              type="radio"
+              name="blood"
+              label="A"
+              value="A"
+              isInvalid={!!formik.errors.blood}
+              checked={formik.values.blood === 'A'}
+            />
+            <Form.Check
+              onChange={formik.handleChange}
+              id="blood-b"
+              type="radio"
+              name="blood"
+              label="B"
+              value="B"
+              isInvalid={!!formik.errors.blood}
+              checked={formik.values.blood === 'B'}
+            />
+            <Form.Check
+              onChange={formik.handleChange}
+              id="blood-ab"
+              type="radio"
+              name="blood"
+              label="AB"
+              value="AB"
+              isInvalid={!!formik.errors.blood}
+              checked={formik.values.blood === 'AB'}
+            />
+            <Form.Check
+              onChange={formik.handleChange}
+              id="blood-o"
+              type="radio"
+              name="blood"
+              label="O"
+              value="O"
+              isInvalid={!!formik.errors.blood}
+              checked={formik.values.blood === 'O'}
+            />
 
-                  {!!errors.blood ? <span style={{ color: '#dc3545' }}>{errors.blood}</span> : null}
-                </div>
-              </Form.Group>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>Berapakah berat badan anda</Form.Label>
-                <Form.Check
-                  type="checkbox"
-                  name="min_weight"
-                  label="Mempunyai Berat Badan Minimal 55 Kg"
-                  onChange={handleChange}
-                  isInvalid={!!errors.min_weight}
-                  feedback={errors.min_weight}
-                  id="min_weight_id"
-                  checked={values.min_weight}
-                />
-              </Form.Group>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>Jenis Kelamin</Form.Label>
-                <Form.Control
-                  name="sex"
-                  onChange={handleChange}
-                  as="select"
-                  aria-label="Jenis Kelamin">
-                  <option value="">Pilih salah satu</option>
-                  <option value="M" selected={values.sex === 'M'}>
-                    Laki-Laki
-                  </option>
-                  <option value="L" selected={values.sex === 'L'}>
-                    Perempuan belum pernah Hamil dan di transfusi darah
-                  </option>
-                </Form.Control>
-                <Form.Control.Feedback type="invalid">{errors.sex}</Form.Control.Feedback>
-              </Form.Group>
-              <Form.Group className="mb-4">
-                <Form.Label>
-                  Pernah menderita COVID-19 dan dibuktikan dengan Swab PCR/ Antigen dengan hasil
-                  Positif ?
-                </Form.Label>
-                <Form.Check
-                  required
-                  type="checkbox"
-                  name="positive_covid"
-                  label="Ya, pernah"
-                  onChange={handleChange}
-                  isInvalid={!!errors.positive_covid}
-                  feedback={errors.positive_covid}
-                  id="positive_covid_id"
-                  checked={values.positive_covid}
-                />
-              </Form.Group>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>
-                  Sudah dinyatakan sembuh dari COVID-19 (dibuktikan dengan hasil Swab Negatif/ surat
-                  pernyataan sembuh dari dokter/ RS) dan tidak mengalami gejala selama 14 hari ?
-                </Form.Label>
-                <div>
-                  <Form.Check
-                    onChange={handleChange}
-                    id="recovered_covid-yes"
-                    type="radio"
-                    name="recovered_covid"
-                    label="Ya"
-                    value="YES"
-                    checked={values.recovered_covid === 'YES'}
-                  />
-                  <Form.Check
-                    onChange={handleChange}
-                    id="recovered_covid-no"
-                    type="radio"
-                    name="recovered_covid"
-                    label="Tidak"
-                    value="NO"
-                    checked={values.recovered_covid === 'NO'}
-                  />
-                </div>
-              </Form.Group>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>
-                  Kondisi sehat, cukup istirahat, siap menjadi pendonor plasma konvalesen ?
-                </Form.Label>
-                <div>
-                  <Form.Check
-                    onChange={handleChange}
-                    id="is_healthy-yes"
-                    type="radio"
-                    name="is_healthy"
-                    label="Ya"
-                    value="YES"
-                    checked={values.is_healthy === 'YES'}
-                  />
-                  <Form.Check
-                    onChange={handleChange}
-                    id="is_healthy-no"
-                    type="radio"
-                    name="is_healthy"
-                    label="Tidak"
-                    value="NO"
-                    checked={values.is_healthy === 'NO'}
-                  />
-                </div>
-              </Form.Group>
-              <Form.Group md="4" className="mb-4">
-                <Form.Label>
-                  Siap mendonorkan plasma darah secara sukarela ke PMI Kota Yogyakarta ?
-                </Form.Label>
-                <div>
-                  <Form.Check
-                    onChange={handleChange}
-                    id="is_ready_donor-yes"
-                    type="radio"
-                    name="is_ready_donor"
-                    label="Ya"
-                    value="YES"
-                    isInvalid={!!errors.is_ready_donor}
-                    checked={values.is_ready_donor === 'YES'}
-                  />
-                  <Form.Check
-                    onChange={handleChange}
-                    id="is_ready_donor-no"
-                    type="radio"
-                    name="is_ready_donor"
-                    label="Tidak"
-                    value="NO"
-                    isInvalid={!!errors.is_ready_donor}
-                    checked={values.is_ready_donor === 'NO'}
-                  />
-                  {!!errors.blood ? (
-                    <span style={{ color: '#dc3545' }}>{errors.is_ready_donor}</span>
-                  ) : null}
-                </div>
-              </Form.Group>
-              <Button type="submit">Submit</Button>
-            </Form>
-          );
-        }}
-      </Formik>
+            {!!formik.errors.blood ? (
+              <span style={{ color: '#dc3545' }}>{formik.errors.blood}</span>
+            ) : null}
+          </div>
+        </Form.Group>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>Berapakah berat badan anda</Form.Label>
+          <Form.Check
+            type="checkbox"
+            name="min_weight"
+            label="Mempunyai Berat Badan Minimal 55 Kg"
+            onChange={formik.handleChange}
+            isInvalid={!!formik.errors.min_weight}
+            feedback={formik.errors.min_weight}
+            id="min_weight_id"
+            checked={formik.values.min_weight}
+          />
+        </Form.Group>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>Jenis Kelamin</Form.Label>
+          <Form.Control
+            name="sex"
+            onChange={formik.handleChange}
+            as="select"
+            aria-label="Jenis Kelamin">
+            <option value="">Pilih salah satu</option>
+            <option value="M" selected={formik.values.sex === 'M'}>
+              Laki-Laki
+            </option>
+            <option value="L" selected={formik.values.sex === 'L'}>
+              Perempuan belum pernah Hamil dan di transfusi darah
+            </option>
+          </Form.Control>
+          <Form.Control.Feedback type="invalid">{formik.errors.sex}</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="mb-4">
+          <Form.Label>
+            Pernah menderita COVID-19 dan dibuktikan dengan Swab PCR/ Antigen dengan hasil Positif ?
+          </Form.Label>
+          <Form.Check
+            required
+            type="checkbox"
+            name="positive_covid"
+            label="Ya, pernah"
+            onChange={formik.handleChange}
+            isInvalid={!!formik.errors.positive_covid}
+            feedback={formik.errors.positive_covid}
+            id="positive_covid_id"
+            checked={formik.values.positive_covid}
+          />
+        </Form.Group>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>
+            Sudah dinyatakan sembuh dari COVID-19 (dibuktikan dengan hasil Swab Negatif/ surat
+            pernyataan sembuh dari dokter/ RS) dan tidak mengalami gejala selama 14 hari ?
+          </Form.Label>
+          <div>
+            <Form.Check
+              onChange={formik.handleChange}
+              id="recovered_covid-yes"
+              type="radio"
+              name="recovered_covid"
+              label="Ya"
+              value="YES"
+              checked={formik.values.recovered_covid === 'YES'}
+            />
+            <Form.Check
+              onChange={formik.handleChange}
+              id="recovered_covid-no"
+              type="radio"
+              name="recovered_covid"
+              label="Tidak"
+              value="NO"
+              checked={formik.values.recovered_covid === 'NO'}
+            />
+          </div>
+        </Form.Group>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>
+            Kondisi sehat, cukup istirahat, siap menjadi pendonor plasma konvalesen ?
+          </Form.Label>
+          <div>
+            <Form.Check
+              onChange={formik.handleChange}
+              id="is_healthy-yes"
+              type="radio"
+              name="is_healthy"
+              label="Ya"
+              value="YES"
+              checked={formik.values.is_healthy === 'YES'}
+            />
+            <Form.Check
+              onChange={formik.handleChange}
+              id="is_healthy-no"
+              type="radio"
+              name="is_healthy"
+              label="Tidak"
+              value="NO"
+              checked={formik.values.is_healthy === 'NO'}
+            />
+          </div>
+        </Form.Group>
+        <Form.Group md="4" className="mb-4">
+          <Form.Label>
+            Siap mendonorkan plasma darah secara sukarela ke PMI Kota Yogyakarta ?
+          </Form.Label>
+          <div>
+            <Form.Check
+              onChange={formik.handleChange}
+              id="is_ready_donor-yes"
+              type="radio"
+              name="is_ready_donor"
+              label="Ya"
+              value="YES"
+              isInvalid={!!formik.errors.is_ready_donor}
+              checked={formik.values.is_ready_donor === 'YES'}
+            />
+            <Form.Check
+              onChange={formik.handleChange}
+              id="is_ready_donor-no"
+              type="radio"
+              name="is_ready_donor"
+              label="Tidak"
+              value="NO"
+              isInvalid={!!formik.errors.is_ready_donor}
+              checked={formik.values.is_ready_donor === 'NO'}
+            />
+            {!!formik.errors.blood ? (
+              <span style={{ color: '#dc3545' }}>{formik.errors.is_ready_donor}</span>
+            ) : null}
+          </div>
+        </Form.Group>
+        <Button type="submit" disabled={formik.isSubmitting}>
+          Submit
+        </Button>
+      </Form>
     </>
   );
 };
