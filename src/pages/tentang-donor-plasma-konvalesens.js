069837aad4f30160bd11e38851a35515b9c52dd1
Farizal Tri Anugrah <omdjin@gmail.com>
2021-07-19 11:36:57 +0700

    feat(jadi-donatur): submit data to API (#4)

---
+++ src/pages/tentang-donor-plasma-konvalesens.js
@@ -0,0 +1,26 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const TentangKonvalesens = () => {
+  return (
+    <>
+      <Head>
+        <title>Request Plasma Konvalesens</title>
+        <meta name="description" content="Data Donor Plasma Konvalesens" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Apa Itu Donor Plasma Konvalesens</h1>
+      <div className="text-center">
+        <Image
+          src="/assets/tentang-donor-plasma-konvalesens.jpg"
+          alt="syarat"
+          width={590}
+          height={600}
+          layout="intrinsic"
+        />
+      </div>
+    </>
+  );
+};
+export default TentangKonvalesens;
